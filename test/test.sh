105de201d6eaff5b312c0e6602a095a377fc6a8a
Vincent Bernardoff <vb@luminar.eu.org>
2014-08-30 16:38:28 +0200

    Fixing test, adding more arguments

--- test/test.sh
+++ test/test.sh
@@ -7,23 +7,20 @@ control_c ()
     exit 0
 }
 
-if [[ $# -lt 3 ]]
+if [[ $# -lt 6 ]]
 then
-    printf "Usage: ${0} <iface> <node_port> <group_port>\n"
+    printf "Usage: ${0} <node_addr> <node_port> <group_addr> <group_port> <iface> <connect_addr> \n"
     exit 1
 else
     trap control_c SIGINT
-    ./llsqlite.native -tls -server ::/${2}/ff02::dead:beaf/${3}/${1} -vv llsqlite3 -vv llsqlite "0" "llsqlite.db" &
-    ./llsqlite.native -tls -server ::/$(($2 + 2))/ff02::dead:beaf/${3}/${1} -vv llsqlite3 -vv llsqlite "1" "llsqlite.db2" &
-    ./llsqlite.native -tls -server ::/$(($2 + 4))/ff02::dead:beaf/${3}/${1} -vv llsqlite3 -vv llsqlite "2" "llsqlite.db3" &
-    # ./llsqlite.native -tls -server ::/$(($2 + 6))/ff02::dead:beaf/${3}/${1} -vv llsqlite3 &
-    # ./llsqlite.native -tls -server ::/$(($2 + 8))/ff02::dead:beaf/${3}/${1} -vv llsqlite3 &
-    # ./llsqlite.native -tls -server ::/$(($2 + 10))/ff02::dead:beaf/${3}/${1} -vv llsqlite3 &
+    ./llsqlite.native -tls -server ${1}/${2}/${3}/${4}/${5} -vv llsqlite3 -vv llsqlite "0" "llsqlite.db" &
+    ./llsqlite.native -tls -server ${1}/$(($2 + 2))/${3}/${4}/${5} -vv llsqlite3 -vv llsqlite "1" "llsqlite.db2" &
+    ./llsqlite.native -tls -server ${1}/$(($2 + 4))/${3}/${4}/${5} -vv llsqlite3 -vv llsqlite "2" "llsqlite.db3" &
     sleep 3
     RC=1
     while [[ $RC -ne 0 ]]; do
     	printf "Trying to create table STORE\n"
-	./llsqlite.native -tls -client ::1/10000 "CREATE TABLE STORE (KEY STRING, VALUE STRING)"
+	./llsqlite.native -tls -client ${6}/10000 "CREATE TABLE STORE (KEY STRING, VALUE STRING)"
     	RC=$?
     	sleep 1
     done
@@ -33,7 +30,7 @@ else
     	RC=1
     	while [[ $RC -ne 0 ]]; do
     	    printf "Trying to insert $K -> $V into the DB\n"
-	    ./llsqlite.native -tls -client ::1/10000 "INSERT INTO STORE (KEY, VALUE) VALUES (\"`pwgen -1`\", \"`pwgen -1`\")"
+	    ./llsqlite.native -tls -client ${6}/10000 "INSERT INTO STORE (KEY, VALUE) VALUES (\"`pwgen -1`\", \"`pwgen -1`\")"
     	    RC=$?
     	    sleep 1
     	done
